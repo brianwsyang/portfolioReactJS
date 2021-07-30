5b710fbb1d8f7055cc74d585af79ec92ae45484d
Brian Yang <brianwsyang@berkeley.edu>
2021-07-30 12:04:26 -0700

    30Jul2021.v5: started adding list of courses in Courses.js

---
+++ src/components/Courses.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Courses.css'
+
+const Courses = () => {
+
+  const courses = [
+    { name: 'Structure & Interpretation of Computer Programs' },
+    { name: 'Data Structures' },
+    { name: 'Data Analytics & Mining'},
+    { name: 'Principles & Techniques of Data Science' },
+    { name: 'Foundations of Data Science' },
+    { name: 'Introduction to Machine Learning & Data Analytics' },
+    { name: 'Introduction to Artificial Intelligence' },
+    { name: 'Natural Language Processing' },
+    { name: 'Computational Models of Cognition' },
+
+
+  ];
+
+  return (<>
+  
+  
+
+  </>)
+}
+
+export default Courses
